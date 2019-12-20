20b715386391fd3165986640873dbdfb2aca76b5
Trevor McCann <trevormccann21@gmail.com>
2019-12-19 20:32:24 -0800

    add new responses and update response strings

--- lambda/custom/languageStrings.js
+++ lambda/custom/languageStrings.js
@@ -1,20 +1,21 @@
 /* *
- * We create a language strings object containing all of our strings.
- * The keys for each string will then be referenced in our code, e.g. handlerInput.t('WELCOME_MSG').
- * The localisation interceptor in index.js will automatically choose the strings
- * that match the request's locale.
- * */
+* We create a language strings object containing all of our strings.
+* The keys for each string will then be referenced in our code, e.g. handlerInput.t('WELCOME_MSG').
+* The localisation interceptor in index.js will automatically choose the strings
+* that match the request's locale.
+* */
 
 module.exports = {
-    en: {
-        translation: {
-            WELCOME_MSG: 'Welcome, you can say Hello or Help. Which would you like to try?',
-            HELLO_MSG: 'Hello World!',
-            HELP_MSG: 'You can say hello to me! How can I help?',
-            GOODBYE_MSG: 'Goodbye!',
-            REFLECTOR_MSG: 'You just triggered {{intentName}}',
-            FALLBACK_MSG: 'Sorry, I don\'t know about that. Please try again.',
-            ERROR_MSG: 'Sorry, I had trouble doing what you asked. Please try again.'
-        }
-    }
-}
+  en: {
+    translation: {
+      WELCOME_MSG: 'Welcome, try asking for the next bus by saying next bus',
+      NEXT_BUS_MSG: 'The next bus at stop {stopNumber} is the {busNumber} at {time}',
+      HELP_MSG: 'You can say hello to me! How can I help?',
+      GOODBYE_MSG: 'Goodbye!',
+      REFLECTOR_MSG: 'You just triggered {{intentName}}',
+      FALLBACK_MSG: 'Sorry, I don\'t know about that. Please try again.',
+      ERROR_MSG: 'Sorry, I had trouble doing what you asked. Please try again.',
+      REQUEST_PERMISSION_MSG: 'Please enable device address information through the alexa app',
+    },
+  },
+};
